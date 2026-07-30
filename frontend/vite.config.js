741178129a22e8d45dedb2d3b8cddcd3581f2467
rteex <risto.teerilahti@outlook.com>
2026-07-30 11:57:57 +0300

    Share - for real

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,15 @@ export default defineConfig({
         background_color: '#0f1612',
         display:          'standalone',
         start_url:        '/',
+        share_target: {
+          action: '/share',
+          method: 'GET',
+          params: {
+            text:  'text',
+            title: 'title',
+            url:   'url',
+          }
+        },
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
